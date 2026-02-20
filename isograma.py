2a21137f5b72e719671bed6a76ebb0267defc176
Vyreath <victorxpert97@gmail.com>
2026-02-20 14:44:04 -0600

    Update isograma.py

--- isograma.py
+++ isograma.py
@@ -3,12 +3,14 @@ esisograma = True
 
 for i in range (0,len(palabra)):
 
-    letra = palabra[i]
+    letra = palabra[i].lower()
 
     for j in range (0,len(palabra)):
 
-        if (letra == palabra[j]) and i != j :
+        if letra == palabra[j].lower() and i != j :
             esisograma = False
             break
+    if esisograma == False:
+        break
 
 print(esisograma)
